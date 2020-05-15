a055714a5d679db64f1ae8739ac746abf4fcd95c
jamiscript <jah.passaro@gmail.com>
2020-05-14 23:54:30 -0300

    updated routes format

--- coronasavior/App.js
+++ coronasavior/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import Router from './router.js';
-import { StyleSheet, Text } from 'react-native';
+import Routes from './router.js';
+
 
 export default function App() {
   return (
-    <Router />
+    <Routes/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ coronasavior/routes/stackNavigation.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { lightGreen } from '../utils/constants';
+
+import Login from '../screens/login';
+import SignUp from '../screens/signup';
+import Profile from '../screens/profile';
+import Home from '../screens/home';
+
+const Stack = createStackNavigator()
+
+function stackNav() {
+  return (
+    <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+         headerTintColor: '#fff',
+         headerBackTitleVisible: false,
+         headerStyle: {
+             backgroundColor: lightGreen,
+         }
+        }}
+    >
+    <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+            headerShown: true,
+            headerLeft: () => null
+        }}
+    />
+    <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+            headerShown: true,
+            headerLeft: () => null
+        }}
+    />
+    <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+            headerShown: true,
+            headerLeft: () => null
+        }}
+    />
+    <Stack.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+            headerShown: true,
+            headerLeft: () => null
+        }}
+    />
+  </Stack.Navigator>
+  )
+}
+
+export default stackNav
